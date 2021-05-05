2a52d0a29e9f27c7c11009f9f7d114eab275b0b4
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-05-04 18:05:30 -0700

    added dogs

---
+++ data/dogs.js
@@ -0,0 +1,68 @@
+// example seed data
+const dogs = [
+  {
+    name: 'Scooby Doo',
+    type: 'Great Dane',
+    url: '',
+    year: 1969,
+    tvShow: 'Scooby Doo, Where are you?',
+    isSidekick: true
+  },
+  {
+    name: 'Pluto',
+    type: 'Bloodhound',
+    url: '',
+    year: 1931,
+    tvShow: 'Mickey Mouse Clubhouse',
+    isSidekick: true
+  },
+  {
+    name: 'Bolt',
+    type: 'German Shepherd',
+    url: '',
+    year: 2008,
+    tvShow: Bolt,
+    isSidekick: false
+  },
+  {
+    name: 'Snoopy',
+    type: 'Beagle',
+    url: '',
+    year: 1978,
+    tvShow: 'Charlie Brown and Snoopy',
+    isSidekick: true
+  },
+  {
+    name: 'Odie',
+    type: 'Dachshund & Terrier Mix',
+    url: '',
+    year: 1978,
+    tvShow: 'The Garfield Show',
+    isSidekick: true
+  },
+  {
+    name: 'Tramp',
+    typ: 'Schnauzer mix',
+    url: '',
+    year: 1914,
+    tvShow: 'The Lady and the Tramp',
+    isSidekick: false
+  },
+  {
+    name: 'Goofy',
+    type: 'Coonhound',
+    url: '',
+    year: 1939,
+    tvShow: 'Goof Troup',
+    isSidekick: true
+  },
+  {
+    name: 'Scrappy Doo',
+    type: 'Great Dane',
+    url: '',
+    year: 1979,
+    tvShow: 'Scooby Doo',
+    isSidekick: true
+  }
+];
+export default dogs;
