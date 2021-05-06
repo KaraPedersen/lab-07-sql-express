b650514b2b6ff72f7ab77e277d2bc5148773fc0b
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-05-05 17:07:07 -0700

    skipped test and added to passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,93 +7,119 @@ const request = supertest(app);
 
 describe('API Routes', () => {
 
+  // beforeAll(() => {
+  //   execSync('npm run setup-db');
+  // });
   beforeAll(() => {
-    execSync('npm run setup-db');
+    execSync('npm run recreate-tables');
   });
 
   afterAll(async () => {
     return client.end();
   });
 
-  const expectedDogs = [
-    {
-      id: expect.any(Number),
-      name: 'Scooby Doo',
-      type: 'Great Dane',
-      url: 'scooby-doo.jpeg',
-      year: 1969,
-      tvShow: 'Scooby Doo, Where are you?',
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Pluto',
-      type: 'Bloodhound',
-      url: '',
-      year: 1931,
-      tvShow: 'Mickey Mouse Clubhouse',
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Bolt',
-      type: 'German Shepherd',
-      url: '',
-      year: 2008,
-      tvShow: 'Bolt',
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Snoopy',
-      type: 'Beagle',
-      url: '',
-      year: 1978,
-      tvShow: 'Charlie Brown and Snoopy',
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Odie',
-      type: 'Dachshund & Terrier Mix',
-      url: '',
-      year: 1978,
-      tvShow: 'The Garfield Show',
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Tramp',
-      type: 'Schnauzer mix',
-      url: '',
-      year: 1914,
-      tvShow: 'The Lady and the Tramp',
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Goofy',
-      type: 'Coonhound',
-      url: '',
-      year: 1939,
-      tvShow: 'Goof Troup',
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Scrappy Doo',
-      type: 'Great Dane',
-      url: '',
-      year: 1979,
-      tvShow: 'Scooby Doo',
-      isSidekick: true
-    }
-  ];
+  // const expectedDogs = [
+  let ScoobyDoo = {
+    id: expect.any(Number),
+    name: 'Scooby Doo',
+    type: 'Great Dane',
+    url: 'scooby-doo.jpeg',
+    year: 1969,
+    tvShow: 'Scooby Doo, Where are you?',
+    isSidekick: true
+  };
+  let pluto = {
+    id: expect.any(Number),
+    name: 'Pluto',
+    type: 'Bloodhound',
+    url: '',
+    year: 1931,
+    tvShow: 'Mickey Mouse Clubhouse',
+    isSidekick: true
+  };
+  let bolt = {
+    id: expect.any(Number),
+    name: 'Bolt',
+    type: 'German Shepherd',
+    url: '',
+    year: 2008,
+    tvShow: 'Bolt',
+    isSidekick: false
+  };
+  let snoopy = {
+    id: expect.any(Number),
+    name: 'Snoopy',
+    type: 'Beagle',
+    url: '',
+    year: 1978,
+    tvShow: 'Charlie Brown and Snoopy',
+    isSidekick: true
+  };
+  let odie = {
+    id: expect.any(Number),
+    name: 'Odie',
+    type: 'Dachshund & Terrier Mix',
+    url: '',
+    year: 1978,
+    tvShow: 'The Garfield Show',
+    isSidekick: true
+  };
+  let tramp = {
+    id: expect.any(Number),
+    name: 'Tramp',
+    type: 'Schnauzer mix',
+    url: '',
+    year: 1914,
+    tvShow: 'The Lady and the Tramp',
+    isSidekick: false
+  };
+  let goofy = {
+    id: expect.any(Number),
+    name: 'Goofy',
+    type: 'Coonhound',
+    url: '',
+    year: 1939,
+    tvShow: 'Goof Troup',
+    isSidekick: true
+  };
+  let scrappyDoo = {
+    id: expect.any(Number),
+    name: 'Scrappy Doo',
+    type: 'Great Dane',
+    url: '',
+    year: 1979,
+    tvShow: 'Scooby Doo',
+    isSidekick: true
+  };
+
+  it('POST tramp to /api/dogs', async () => {
+    const response = await request
+      .post('/api/dogs')
+      .send(tramp);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(tramp);
+
+    tramp = response.body;
+  });
+
+  it('PUT updated tramp to /api/cats/:id', async () => {
+    tramp.tvShow = 'The Lady and the Tramp';
+    tramp.name = 'tramp';
+
+    const response = await request
+      .put(`/api/dogs/${tramp.id}`)
+      .send(tramp);
+
+  });
+
+
+
 
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('GET /api/dogs', async () => {
+  it.skip('GET /api/dogs', async () => {
     // act - make the request
     const response = await request.get('/api/dogs');
 
@@ -105,10 +131,24 @@ describe('API Routes', () => {
 
   });
 
+  // it('GET list of dogs from /api/dogs', async () => {
+  //   const r1 = await (await request.post('/api/dogs')).send(snoopy);
+  //   snoopy = r1.body;
+  //   const r2 = await (await request.post('/api/dogs')).send(goofy);
+  //   goofy = r2.body;
+
+  //   const response = await request.get('/api/dogs');
+
+  //   expect(response.status).toBe(200);
+  //   expect(response.body).toEqual(expect.arrayContaining([tramp, snoopy, goofy]));
+  // });
+
+
+
   // If a GET request is made to /api/cats/:id, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/dogs/:id', async () => {
+  it.skip('GET /api/dogs/:id', async () => {
     const response = await request.get('/api/dogs/2');
     expect(response.status).toBe(200);
     expect(response.body).toEqual(expectedDogs[1]);
